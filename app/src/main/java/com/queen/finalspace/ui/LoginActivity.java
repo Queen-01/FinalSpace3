1fc27c34b1ddc3c23cfe35f11f531b05456c271e
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-22 15:04:20 +0300

    made changes

--- app/src/main/java/com/queen/finalspace/ui/LoginActivity.java
+++ app/src/main/java/com/queen/finalspace/ui/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 import com.queen.finalspace.R;
 
@@ -14,6 +15,7 @@ import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     @BindView(R.id.registerTextView) TextView mRegisterTextView;
+    @BindView(R.id.passwordLoginButton) Button mPasswordLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +28,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view){
         if (view == mRegisterTextView){
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            Intent intent = new Intent(LoginActivity.this,  CreateAccountActivity.class);
             startActivity(intent);
-            finish();
         }
     }
+
 }
