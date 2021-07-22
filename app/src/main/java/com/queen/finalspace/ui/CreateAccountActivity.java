7ca421faf234f7502cc06abc87abc0d5b97fb55c
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-07-22 12:47:36 +0300

    made changes

--- app/src/main/java/com/queen/finalspace/ui/CreateAccountActivity.java
+++ app/src/main/java/com/queen/finalspace/ui/CreateAccountActivity.java
@@ -75,9 +75,20 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 });
     }
 
+//    private void createAuthStateListener(){
+//        mAuthListener = firebaseAuth -> {
+//            final FirebaseUser user = firebaseAuth.getCurrentUser();
+//            if (user != null){
+//                Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                startActivity(intent);
+//                finish();
+//            }
+//        };
+//    }
     private void createAuthStateListener(){
         mAuthListener = firebaseAuth -> {
-            final FirebaseUser user = firebaseAuth.getCurrentUser();
+            final FirebaseAuth user = firebaseAuth.getCurrentUser();
             if (user != null){
                 Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
